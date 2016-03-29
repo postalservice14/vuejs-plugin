e31bc0ad739a9591397ad1380adbc1a400ebe530
John Kelly <john.m.kelly@cdk.com>
2016-03-29 14:46:10 -0700

    Fix plugin

---
+++ src/main/java/org/vuejs/codeInsight/VueJSDirectives.java
@@ -0,0 +1,26 @@
+package org.vuejs.codeInsight;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class VueJSDirectives {
+    private static final List<String> directives = Arrays.asList(
+            "v-text",
+            "v-html",
+            "v-if",
+            "v-show",
+            "v-else",
+            "v-for",
+            "v-on",
+            "v-bind",
+            "v-model",
+            "v-ref",
+            "v-el",
+            "v-pre",
+            "v-cloak"
+    );
+
+    public static List<String> getDirectives() {
+        return directives;
+    }
+}
